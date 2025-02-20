13cdfd841bf3dbf466884e9efe07903fa03327bd
Ilham Ghazali <m.ilhamghazali@gmail.com>
2025-02-20 08:14:00 +0700

    feat: add login and signup page

---
+++ lib/presentation/profile/widgets/education_card.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EducationCard extends StatelessWidget {
+  final String
+      image; 
+  final String major; 
+  final String degree; 
+  final String university; 
+  final String location; 
+  final String year; 
+  const EducationCard({super.key, required this.image, required this.major, required this.degree, required this.university, required this.location, required this.year});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
